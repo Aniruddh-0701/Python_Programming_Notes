30001e3fd29a80102e9963f33713dbb8d08014ec
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2020-12-04 14:38:11 +0530

    Update vector.py

--- vector.py
+++ vector.py
@@ -12,17 +12,30 @@ class Vector(object):
         self.x = x
         self.y = y
         self.z = z
-
-    def magnitude(self):
-        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        self.magnitude = sqrt(x ** 2 + y ** 2 + z ** 2)
 
     def dotProduct(self, obj):
         return ()
 
+    # def __mul__(self, obj):
+    #     x = self.y * obj.z - self.z * obj.y
+    #     y = self.z * obj.x - self.x * obj.z
+    #     z = self.x * obj.y - self.y * obj.x
+    #     return Vector(x, y, z)
+
+    # def __matmul__(self, obj):
+    #     return self.x*obj.x + self.y*obj.y + self.z*obj.z
+
     def __repr__(self):
-        return f"{self.x}i + {self.y}j + {self.z}k"
+        x = f"i" if self.x == 1 else f"-i" if self.x == -1 else f"{self.x}i"
+        y = f" +j" if self.y == 1 else f" -j" if self.y == -1 else f" {self.y:+}j"
+        z = f" +k" if self.z == 1 else f" -k" if self.z == -1 else f" {self.z:+}k"
+        return x + y + z
 
 
 aVector = Vector(x=2, y=3, z=-1)  # 2i + 3j - k
+bVector = Vector(x=1, y=10, z=-3)  # i + 10j - 3k
 print(aVector)
-print(aVector.magnitude())
+print(aVector.magnitude)
+print(aVector * bVector)
+# print(aVector @ bVector)
